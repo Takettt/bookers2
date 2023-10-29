27593e86f97db0ad0bfa93f2f44cf8fcc6e3a0ab
EC2 Default User <ec2-user@ip-172-31-41-43.ap-northeast-1.compute.internal>
2023-10-29 10:47:51 +0000

    Initial Commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,67 @@
+class BooksController < ApplicationController
+  before_action :is_matching_login_user
+  def new
+    @post_image = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+   if @book.save
+     flash[:notice] = "You have created book successfully."
+     redirect_to book_path(@book.id)
+   else
+      @book_image = current_user
+      @books_index = Book.all
+      render :index
+      @books = Book.new
+   end
+  end
+
+  def edit
+    book = Book.find(params[:id])
+    unless book.user_id == current_user.id
+      redirect_to books_path
+    end
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    flash[:notice] = "You have updated book successfully."
+    redirect_to book_path(@book.id)
+  else
+    render :edit
+  end
+end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
+  def index
+    @books = Book.new
+    @book_image = current_user
+    @books_index = Book.all
+    @book = Book.new
+  end
+
+  def show
+    @book = Book.new
+    @book_show = Book.find(params[:id])
+    @user = @book_show.user
+  end
+
+private
+ def is_matching_login_user
+      unless user_signed_in?
+      redirect_to new_user_session_path
+      end
+ end
+ def book_params
+    params.require(:book).permit(:title, :body)
+ end
+end
